f61ff369d8d61b3b26131b67cbb3eb171444d269
Xandarian <FireCraftpixo@gmail.com>
2024-04-01 23:11:22 +0200

    Added the Modules to the Main File

--- src/main/java/me/pixel/Pixel.java
+++ src/main/java/me/pixel/Pixel.java
@@ -1,7 +1,12 @@
 package me.pixel;
 
+import com.mojang.datafixers.types.templates.Check;
 import com.mojang.logging.LogUtils;
+import me.pixel.commands.*;
+import me.pixel.modules.*;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.commands.Command;
+import meteordevelopment.meteorclient.commands.Commands;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
@@ -17,10 +22,32 @@ public class Pixel extends MeteorAddon {
         LOG.info("Initializing Meteor Pixel Addon");
 
         // Modules
-        //Modules.get().add(new ModuleExample());
+        Modules.get().add(new AntiSpawnpoint());
+        Modules.get().add(new AutoChunkBan());
+        Modules.get().add(new AutoGold());
+        Modules.get().add(new AutoSnowball());
+        Modules.get().add(new AutoTrade());
+        Modules.get().add(new GhostMode());
+        Modules.get().add(new Girlboss());
+        Modules.get().add(new Groupmessage());
+        Modules.get().add(new MinecartAura());
+        Modules.get().add(new MinecartSpeed());
+        Modules.get().add(new MinecartAura());
+        Modules.get().add(new NoSwing());
+        Modules.get().add(new PacketLogger());
+        Modules.get().add(new SitModule());
+        Modules.get().add(new WorldGuardBypass());
+
 
         // Commands
-        //Commands.add(new CommandExample());
+        Commands.add((new CheckCMD()));
+        Commands.add((new ClearInventoryCommand()));
+        Commands.add((new Crash()));
+        Commands.add((new Ping()));
+        Commands.add((new ReloadCapes()));
+        Commands.add((new Title()));
+        Commands.add((new TrashCommand()));
+        Commands.add((new Tts()));
 
         // HUD
         //Hud.get().register(HudExample.INFO);
